7a0b422c3262caa54c5822388bc3f87f4f4e91f6
Brokenprogrammer <brokenprogrammer@gmail.com>
2016-06-02 17:01:58 +0200

    moved database connection test to a EJB.

--- src/java/tb/bmanager/main/IndexManagedBean.java
+++ src/java/tb/bmanager/main/IndexManagedBean.java
@@ -6,12 +6,11 @@
 package tb.bmanager.main;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.sql.Statement;
 import javax.inject.Named;
 import javax.enterprise.context.Dependent;
+import tb.bmanager.auth.RegisterActionBean;
 
 /**
  *
@@ -20,7 +19,9 @@ import javax.enterprise.context.Dependent;
 @Named(value = "indexManagedBean")
 @Dependent
 public class IndexManagedBean {
-
+    
+    RegisterActionBean register = new RegisterActionBean();
+    
     Connection conn = null;
     Statement stmt = null;
     ResultSet rs = null;
@@ -29,69 +30,7 @@ public class IndexManagedBean {
      * Creates a new instance of IndexManagedBean
      */
     public IndexManagedBean() {
-        //THIS WORKS.
-        try{
-            Class.forName("com.mysql.jdbc.Driver").newInstance();
-            
-            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/javaee?" +
-                    "user=root&password=qwer1234");
-            
-        } catch (Exception ex) {
-            System.out.println("Exception: " + ex.getMessage());
-        } finally {
-            if (conn != null) {
-               try {
-                    stmt = conn.createStatement();
-                    rs = stmt.executeQuery("SELECT * FROM USER");
-
-                    // or alternatively, if you don't know ahead of time that
-                    // the query will be a SELECT...
-
-                    //if (stmt.execute("SELECT foo FROM bar")) {
-                    //    rs = stmt.getResultSet();
-                    //}
-
-                    // Now do something with the ResultSet ....
-                    while(rs.next()){
-                        //Retrieve by column name
-                        int id  = rs.getInt("id");
-                        String first = rs.getString("user_type");
-                        String last = rs.getString("username");
-
-                        //Display values
-                        System.out.print("ID: " + id);
-                        System.out.print(", User Type: " + first);
-                        System.out.println(", Username: " + last);
-                     }
-                } catch (SQLException ex){
-                    // handle any errors
-                    System.out.println("SQLException: " + ex.getMessage());
-                    System.out.println("SQLState: " + ex.getSQLState());
-                    System.out.println("VendorError: " + ex.getErrorCode());
-                } finally {
-                    // it is a good idea to release
-                    // resources in a finally{} block
-                    // in reverse-order of their creation
-                    // if they are no-longer needed
-
-                    if (rs != null) {
-                        try {
-                            rs.close();
-                        } catch (SQLException sqlEx) { } // ignore
-
-                        rs = null;
-                    }
-
-                    if (stmt != null) {
-                        try {
-                            stmt.close();
-                        } catch (SQLException sqlEx) { } // ignore
-
-                        stmt = null;
-                    }
-                }
-            }
-        }
+        register.getConnection();
     }
     
     public String helloMsg(){
