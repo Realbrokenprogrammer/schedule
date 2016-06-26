cfff05525853f632410a01793adbc1822702c3dd
Brokenprogrammer <brokenprogrammer@gmail.com>
2016-06-26 16:55:40 +0200

    Added slick library for carousel since there is issues with primefaces.

---
+++ web/resources/js/main.js
@@ -0,0 +1,13 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+$(document).ready(function(){
+  $('.your-class').slick({
+    accessibility: true,
+    arrows: true,
+    mobileFirst: true,
+    dots: true
+  });
+});
