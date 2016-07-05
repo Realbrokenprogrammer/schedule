684cef66e9e5f1a3e8a162d58e1cfa4fac51aaa5
Brokenprogrammer <brokenprogrammer@gmail.com>
2016-07-05 16:05:50 +0200

    Set up angular routes and added overview page for projects.

--- web/resources/js/app/app.js
+++ web/resources/js/app/app.js
@@ -5,4 +5,42 @@
  */
 
  // Define the `BManager` module
-angular.module('BManager', []);
+var BManagerApp = angular.module('BManager', ['ngRoute']);
+
+// configure our routes
+BManagerApp.config(function($routeProvider) {
+    $routeProvider
+
+        // route for the home page
+        .when('/', {
+            templateUrl : 'views/overview.xhtml',
+            controller  : 'mainController'
+        })
+
+        // route for the about page
+        .when('/project/create', {
+            templateUrl : 'views/project/create.xhtml',
+            controller  : 'aboutController'
+        })
+
+        // route for the contact page
+        .when('/contact', {
+            templateUrl : 'pages/contact.html',
+            controller  : 'contactController'
+        });
+});
+
+// create the controller and inject Angular's $scope
+BManagerApp.controller('mainController', function($scope) {
+
+    // create a message to display in our view
+    $scope.message = 'Here you will have your dashboard and overview of all projects!';
+});
+
+BManagerApp.controller('aboutController', function($scope) {
+    $scope.message = 'Look! I am an about page.';
+});
+
+BManagerApp.controller('contactController', function($scope) {
+    $scope.message = 'Contact us! JK. This is just a demo.';
+});
