0ad760e80717b732df890479f6f431c474a78259
Oskar Mendel <brokenprogrammer@gmail.com>
2018-11-01 18:09:53 +0100

    Removed Home and About pages.

--- src/Types.fs
+++ src/Types.fs
@@ -4,10 +4,8 @@ open Global
 
 type Msg =
   | ScheduleMsg of Schedule.Types.Msg
-  | HomeMsg of Home.Types.Msg
 
 type Model = {
     currentPage: Page
-    home: Home.Types.Model
     schedule: Schedule.Types.Model
   }
